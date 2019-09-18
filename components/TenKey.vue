bd1ad7c715273b0c99c5f6d4a5b8d81e6fb44d70
labocho <labocho@penguinlab.jp>
2019-09-18 20:28:25 +0900

    support mouse

--- components/TenKey.vue
+++ components/TenKey.vue
@@ -1,27 +1,27 @@
 <template lang="pug">
   div
     .row(style="height: 25%")
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(7)") 7
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(8)") 8
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(9)") 9
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(7)" @mousedown.prevent="onTapNumkey(7)") 7
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(8)" @mousedown.prevent="onTapNumkey(8)") 8
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(9)" @mousedown.prevent="onTapNumkey(9)") 9
       a.col-3.tenkey-key
-        font-awesome-icon(icon="chevron-up" @touchstart.prevent="onClickAdd(1)")
+        font-awesome-icon(icon="chevron-up" @touchstart.prevent="onTapAdd(1)" @mousedown.prevent="onTapAdd(1)")
     .row(style="height: 25%")
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(4)") 4
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(5)") 5
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(6)") 6
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(4)" @mousedown.prevent="onTapNumkey(4)") 4
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(5)" @mousedown.prevent="onTapNumkey(5)") 5
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(6)" @mousedown.prevent="onTapNumkey(6)") 6
       a.col-3.tenkey-key
-        font-awesome-icon(icon="chevron-down" @touchstart.prevent="onClickAdd(-1)")
+        font-awesome-icon(icon="chevron-down" @touchstart.prevent="onTapAdd(-1)" @mousedown.prevent="onTapAdd(-1)")
     .row(style="height: 25%")
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(1)") 1
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(2)") 2
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(3)") 3
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(1)" @mousedown.prevent="onTapNumkey(1)") 1
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(2)" @mousedown.prevent="onTapNumkey(2)") 2
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(3)" @mousedown.prevent="onTapNumkey(3)") 3
       a.col-3.tenkey-key
     .row(style="height: 25%")
-      a.col-3.tenkey-key(@touchstart.prevent="onClickNumkey(0)") 0
+      a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(0)" @mousedown.prevent="onTapNumkey(0)") 0
       a.col-3.tenkey-key
       a.col-3.tenkey-key
-      a.col-3.tenkey-key(@touchstart.prevent="onClickToggle")
+      a.col-3.tenkey-key(@touchstart.prevent="onTapToggle" @mousedown.prevent="onTapToggle")
         font-awesome-icon(v-if="!isRunning", icon="play")
         font-awesome-icon(v-if="isRunning", icon="pause")
 </template>
@@ -51,15 +51,15 @@ export default {
     },
   },
   methods: {
-    onClickAdd(i) {
+    onTapAdd(i) {
       this.value += i;
       this.$store.dispatch("updateDisplayBpm", this.value);
     },
-    onClickNumkey(i) {
+    onTapNumkey(i) {
       this.value = (this.value * 10 + i) % 1000;
       this.$store.dispatch("updateDisplayBpm", this.value);
     },
-    onClickToggle() {
+    onTapToggle() {
       this.$store.dispatch("toggle");
     },
   },
