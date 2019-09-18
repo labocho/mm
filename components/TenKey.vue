2b8f361731cb6df9826ae67eee2195de2efc3a7c
labocho <labocho@penguinlab.jp>
2019-09-18 20:05:38 +0900

    use pug

--- components/TenKey.vue
+++ components/TenKey.vue
@@ -1,29 +1,23 @@
-<template>
-  <div>
-    <div class="row" style="height: 25%">
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(7)">7</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(8)">8</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(9)">9</a>
-    </div>
-    <div class="row" style="height: 25%">
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(4)">4</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(5)">5</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(6)">6</a>
-    </div>
-    <div class="row" style="height: 25%">
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(1)">1</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(2)">2</a>
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(3)">3</a>
-    </div>
-    <div class="row" style="height: 25%">
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickNumkey(0)">0</a>
-      <a class="col-4 tenkey-key" />
-      <a class="col-4 tenkey-key" @touchstart.prevent="onClickToggle">
-        <font-awesome-icon v-if="!isRunning" icon="play" />
-        <font-awesome-icon v-if="isRunning" icon="pause" />
-      </a>
-    </div>
-  </div>
+<template lang="pug">
+  div
+    .row(style="height: 25%")
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(7)") 7
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(8)") 8
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(9)") 9
+    .row(style="height: 25%")
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(4)") 4
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(5)") 5
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(6)") 6
+    .row(style="height: 25%")
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(1)") 1
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(2)") 2
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(3)") 3
+    .row(style="height: 25%")
+      a.col-4.tenkey-key(@touchstart.prevent="onClickNumkey(0)") 0
+      a.col-4.tenkey-key
+      a.col-4.tenkey-key(@touchstart.prevent="onClickToggle")
+        font-awesome-icon(v-if="!isRunning", icon="play")
+        font-awesome-icon(v-if="isRunning", icon="pause")
 </template>
 
 <script>
