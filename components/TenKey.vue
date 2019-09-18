028c4b27636945b33d1ce46918534c92fb232e61
labocho <labocho@penguinlab.jp>
2019-09-18 21:46:01 +0900

    up/down のタップ可能エリアが文字部分だけだったのを修正

--- components/TenKey.vue
+++ components/TenKey.vue
@@ -4,14 +4,14 @@
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(7)" @mousedown.prevent="onTapNumkey(7)") 7
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(8)" @mousedown.prevent="onTapNumkey(8)") 8
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(9)" @mousedown.prevent="onTapNumkey(9)") 9
-      a.col-3.tenkey-key
-        font-awesome-icon(icon="chevron-up" @touchstart.prevent="onTapAdd(1)" @mousedown.prevent="onTapAdd(1)")
+      a.col-3.tenkey-key(@touchstart.prevent="onTapAdd(1)" @mousedown.prevent="onTapAdd(1)")
+        font-awesome-icon(icon="chevron-up")
     .row(style="height: 25%")
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(4)" @mousedown.prevent="onTapNumkey(4)") 4
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(5)" @mousedown.prevent="onTapNumkey(5)") 5
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(6)" @mousedown.prevent="onTapNumkey(6)") 6
-      a.col-3.tenkey-key
-        font-awesome-icon(icon="chevron-down" @touchstart.prevent="onTapAdd(-1)" @mousedown.prevent="onTapAdd(-1)")
+      a.col-3.tenkey-key(@touchstart.prevent="onTapAdd(-1)" @mousedown.prevent="onTapAdd(-1)")
+        font-awesome-icon(icon="chevron-down")
     .row(style="height: 25%")
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(1)" @mousedown.prevent="onTapNumkey(1)") 1
       a.col-3.tenkey-key(@touchstart.prevent="onTapNumkey(2)" @mousedown.prevent="onTapNumkey(2)") 2
